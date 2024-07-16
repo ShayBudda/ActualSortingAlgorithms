4aed9725c4e433656cb576761d70dcd6ce442e2b
ShayBudda <shayevanswow@gmail.com>
2024-07-16 16:02:00 -0700

    Added Insertion Sort

--- Program.cs
+++ Program.cs
@@ -1,16 +1,38 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace ActualSortingAlgorithms
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            int[] arr1 = { 45, 56, 34, 65, 87, 12, 89 };
-            PrintArray(arr1);
-            BubbleSort(arr1);
-            PrintArray(arr1);
+            int[] arr1 = { 90, 3, 2, 56, 32, 34, 65, 68, 76, 1, 0, 100, 8 };
+            int[] arr2 = { 90, 3, 2, 56, 32, 34, 65, 68, 76, 1, 0, 100, 8 };
+            int[] arr3 = { 90, 3, 2, 56, 32, 34, 65, 68, 76, 1, 0, 100, 8 };
+            int[] arrSorted1 = { 0, 1, 2, 3, 8, 32, 34, 56, 65, 68, 76, 90, 100 };
+            int[] arrSorted2 = { 0, 1, 2, 3, 8, 32, 34, 56, 65, 68, 76, 90, 100 };
+            int[] arrSorted3 = { 0, 1, 2, 3, 8, 32, 34, 56, 65, 68, 76, 90, 100 };
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            InsertionSort(arr1);
+            stopwatch.Stop();
+            Console.WriteLine($"Elapsed time for Insertion Sort: {stopwatch.ElapsedTicks}");
+
+            stopwatch.Restart();
+            stopwatch.Start();
+            BubbleSort(arr2);
+            stopwatch.Stop();
+            Console.WriteLine($"Elapsed time for Bubble Sort: {stopwatch.ElapsedTicks}");
+
+            stopwatch.Restart();
+            stopwatch.Start();
+            SelectionSort(arr3);
+            stopwatch.Stop();
+            Console.WriteLine($"Elapsed time for Selection Sort: {stopwatch.ElapsedTicks}");
         }
+
         public static void PrintArray(int[] arr)
         {
             foreach (int element in arr)
@@ -18,30 +40,90 @@ namespace ActualSortingAlgorithms
                 Console.Write($"{element} ");
             }
             Console.WriteLine();
-        
-        }
-        public static Array BubbleSort(int[] arraytosort)
-        {
-            int temp;
-            //Overall O(n^2) runtime - Big O
-            //
-            for (int i = 0; i < arraytosort.Length - 1; i++) //O(n)how many times it goes through the unsorted array
-            {
-                int swaps = 0;
-                for (int j = 0; j < arraytosort.Length - 1 - i; j++)//O(n)
-                {
-                    if (arraytosort[j] > arraytosort[j + 1])
-                    {
-                        swaps++;
-                        temp = arraytosort[j];
-                        arraytosort[j] = arraytosort[j + 1];
-                        arraytosort[j + 1] = temp;
-                    }
-                }
-                if (swaps == 0) break; // break out of the loop if there are no swaps
-            }
-            return arraytosort;
         }
 
+        public static void InsertionSort(int[] arr)
+        {
+            // A for loop to iterate the array to sort the elements
+            // Overall worst case scenario O(n ^ 2)
+            // Best case scenario O(n)
+            for (int i = 1; i < arr.Length; i++)    // O(n) <--- O(n - 1)
+            {
+                // Temporary store an elements in the 'temp' variable
+                int temp = arr[i];
+
+                // Evaluate the elements to the left of the array with 'temp' element
+                // In this case we start at index 1 and the array started with index 0
+                int priorIndex = i - 1;
+
+                // If out prior element is greater than our stored ('temp') element
+                // and we have not reached the end of the array
+                while (priorIndex >= 0 && arr[priorIndex] > temp)   // O(n)
+                {
+                    arr[priorIndex + 1] = arr[priorIndex];
+                    priorIndex--;
+                }
+
+                arr[priorIndex + 1] = temp;
+
+            }
+            Console.WriteLine();
+        }
+
+        public static int BubbleSort(int[] arrToSort)
+        {
+            int totalOuterIterations = 0;
+            int temp;
+            // Overall O(n^2) runtime - Big O
+            // Big Omega - O(n^2)
+            for (int i = 0; i < arrToSort.Length - 1; i++) // O(n) how many times we need to go though the unsorted array
+            {
+                totalOuterIterations++;
+                int swaps = 0;
+                for (int j = 0; j < arrToSort.Length - 1 - i; j++)  // O(n)
+                {
+                    // we need to swap
+                    if (arrToSort[j] > arrToSort[j + 1])           // can modify if '>' change to '<'
+                    {
+                        swaps++;
+                        temp = arrToSort[j];
+                        arrToSort[j] = arrToSort[j + 1];
+                        arrToSort[j + 1] = temp;
+                    }
+                }
+
+                if (swaps == 0) break; // break out of the for loop
+            }
+
+            return totalOuterIterations;
+        }
+
+        public static void SelectionSort(int[] arrToSort)
+        {
+            // minIndex keeps track of the smallest index in each iteration
+            // temp is used as temporary storage
+            int minIndex, temp;
+
+            // O(n) how many times we need to go though the unsorted array
+            for (int i = 0; i < arrToSort.Length; i++)
+            {
+                minIndex = i; // set the minIdex equal to current smallest index
+                for (int j = i; j < arrToSort.Length; j++) // loop through each element starting at i
+                {
+                    // if the element is smaller than the current minIndex
+                    if (arrToSort[j] < arrToSort[minIndex])
+                    {
+                        // swap
+                        minIndex = j;
+                    }
+                }
+
+                // swap elements
+                // swap current i (which is smallest position with the smallest/min element)
+                temp = arrToSort[i];
+                arrToSort[i] = arrToSort[minIndex];
+                arrToSort[minIndex] = temp;
+            }
+        }
     }
 }
